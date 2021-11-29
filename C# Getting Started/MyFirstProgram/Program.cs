9ca450910b6e9685b02c9a82e04d37f92ef98e55
Iseries2090 <khayden2090@gmail.com>
2021-11-29 14:15:44 -0600

    Updated classes

--- C# Getting Started/MyFirstProgram/Program.cs
+++ C# Getting Started/MyFirstProgram/Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using MyUtilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MyFirstProgram // Note: actual namespace depends on the project name.
+namespace FirstProgram // Note: actual namespace depends on the project name.
 {
+
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello more World!");
-            Console.WriteLine("Hello, third world!");
+            WeatherUtilities.Report("Bologna", 23, 65); //~73F
         }
     }
 }
---
+++ C# Getting Started/MyFirstProgram/WeatherUtilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyUtilities
+{
+	public class WeatherUtilities
+	{
+		public WeatherUtilities()
+		{
+		}
+
+		static float FahrenheitToCelisius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
+        }
+
+		static float CelsiusToFahrenheit ( float temperatureCelsius)
+        {
+            return temperatureCelsius * 1.8f + 32;
+        }
+		//The lower the index the more comfortable it is
+		static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        {
+			//Probably not a very reliable formula
+			
+			return (temperatureFahrenheit + humidityPercent) / 4; 
+		}
+
+		public static void Report(string location, float temperatureCelsius, float humidity)
+        {
+			var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
+			Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
+        }
+	}
+
+
+}
